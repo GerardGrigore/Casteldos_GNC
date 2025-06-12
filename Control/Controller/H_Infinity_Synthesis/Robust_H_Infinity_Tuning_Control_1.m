36830a9f09d9d2525524f2687b1d80abe3f63cd0
GerardGrigore <gerard.bousquet75@gmail.com>
2025-06-12 20:53:43 +0200

    Robust control script updated
    Frequency and margins requirements added to the script

--- Control/Controller/H_Infinity_Synthesis/Robust_H_Infinity_Tuning_Control_1.m
+++ Control/Controller/H_Infinity_Synthesis/Robust_H_Infinity_Tuning_Control_1.m
@@ -22,42 +22,62 @@ Controller_PIDF_Structure = tunablePID('Controller_PIDF','pid');
 % An analysis point are the locations where the loop can be opened. For
 % example, for signal injection about requirements on the system.
 Analysis_Point_Direct_Chain = AnalysisPoint('Analysis_Point_Direct_Chain');
+Analysis_Point_Indirect_Chain = AnalysisPoint('Analysis_Point_Indirect_Chain');
 
 % Connection of the components to build an entire feedback controlled
 % system:
-Closed_Loop = feedback(Rudder_To_Heading_Continuous*Controller_PIDF_Structure,Analysis_Point_Direct_Chain);
+Closed_Loop = feedback(Rudder_To_Heading_Continuous*Analysis_Point_Direct_Chain*Controller_PIDF_Structure,...
+                       Analysis_Point_Indirect_Chain);
 Closed_Loop.InputName = 'Heading_Aimed';
 Closed_Loop.OutputName = 'Heading_Observed';
 
 % Specification of several requirements:
 % Tracking requirement:
 % The response time of the system:
-Time_Step_Response = 10;
+Time_Step_Response = 20;
 % The error of the output:
 Heading_Error_Percent = 0.1;
 Requirement_Tracking = TuningGoal.Tracking('Heading_Aimed','Heading_Observed',Time_Step_Response,Heading_Error_Percent);
 % Rejection requirement:
-% The attenuation of a disturbance injected at Analysis_Point_Direct_Chain 
+% The attenuation of a disturbance injected at Analysis_Point_Indirect_Chain 
 % must be suppressed by a factor of Factor_Suppression_Disturbance.
 Factor_Suppression_Disturbance = 5;
-Requirement_Rejection = TuningGoal.Gain('Analysis_Point_Direct_Chain','Heading_Observed',Factor_Suppression_Disturbance);
+Requirement_Rejection = TuningGoal.Gain('Analysis_Point_Indirect_Chain','Heading_Observed',Factor_Suppression_Disturbance);
+% Gain and phase margin specifications:
+Aimed_Gain_Margin = 30;
+Aimed_Phase_Margin = 70;
+% These margins must be seen at the first Analysis Point in the open-loop:
+Requirement_Margins = TuningGoal.Margins('Analysis_Point_Direct_Chain',Aimed_Gain_Margin,Aimed_Phase_Margin);
 
 % Tuning of the control system:
-Number_Random_Start_Points = 10;
-Termination_Relative_Tolerance = 0.001;
+Number_Random_Start_Points = 20;
+Termination_Relative_Tolerance = 0.01;
 Synthesis_Options = systuneOptions('RandomStart',Number_Random_Start_Points,'Display','iter',...
                                     'SoftTol',Termination_Relative_Tolerance);
-[Closed_Loop_Optimized,Best_Achieved_Soft_Contraint] = systune(Closed_Loop,[Requirement_Tracking,Requirement_Rejection],...
-                                                               [],Synthesis_Options);
+[Closed_Loop_Optimized,Best_Achieved_Soft_Contraint] = systune(Closed_Loop,[Requirement_Tracking,Requirement_Rejection,...
+                                                               Requirement_Margins],[],Synthesis_Options);
+
+% Access to the tuned PID controller variables:
+PIDF_Controller_Tuned_Paramers = getBlockValue(Closed_Loop_Optimized,'Controller_PIDF');
 
 % Temporal validation of the synthetized controller:
 figure;
 stepplot(Closed_Loop_Optimized);
 
 % Check the rejection requirement:
-Closed_Loop_Disturbance = getIOTransfer(Closed_Loop_Optimized,'Analysis_Point_Direct_Chain','Heading_Observed');
+Closed_Loop_Disturbance = getIOTransfer(Closed_Loop_Optimized,'Analysis_Point_Indirect_Chain','Heading_Observed');
 figure;
 stepplot(Closed_Loop_Disturbance);
+
+% Check the alignment with the requirements:
+clf;
+viewGoal([Requirement_Tracking Requirement_Rejection Requirement_Margins],Closed_Loop_Optimized);
+
+% Nichols of the open-loop:
+nichols(Rudder_To_Heading_Continuous);
+hold on;
+nichols(PIDF_Controller_Tuned_Paramers*Rudder_To_Heading_Continuous);
+title('Continuous Ship plant and Open-loop controlled Ship with tuned H∞');
 
 
 
